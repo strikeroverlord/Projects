f86a8dbdc020e6a83ed8d97a18dd92f5ac098757
strikeroverlord <84431400+strikeroverlord@users.noreply.github.com>
2021-05-19 12:34:00 +0800

    Add files via upload

---
+++ Projects/WeSourceExam/WeSourceExam/ApiClient.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WeSourceExam
+{
+    public class ApiClient
+    {   
+        public static void GetRates(string FromDate, string ToDate)
+        {
+            string accesskey = "9379d1e280c8d1f1f4f96d2c288dc3e1";            
+                        
+            var daterange = GenerateDates(DateTime.Parse(FromDate), DateTime.Parse(ToDate));
+
+            foreach(var dt in daterange)
+            {                
+                CallWebAPI(dt.ToString("yyyy-MM-dd"), accesskey).Wait();
+            }                      
+
+        }
+
+        public static List<DateTime> GenerateDates(DateTime FromDate, DateTime ToDate)
+        {                        
+
+            var daterange = Enumerable.Range(0, 1 + ToDate.Subtract(FromDate).Days)
+          .Select(offset => FromDate.AddDays(offset))
+          .ToList();                       
+
+            return daterange;
+            
+        }
+
+        public static async Task CallWebAPI(string historicaldate,string accesskey)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://data.fixer.io/api/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));                
+                HttpResponseMessage response = await client.GetAsync(historicaldate+"?access_key="+accesskey+"&symbols=&format=1");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    JsonModel jsondata = await response.Content.ReadAsAsync<JsonModel>();
+                    ResultModel.RatesDataCollections.AddRange(ConvertJsonDataToRatesList(jsondata));                                        
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }                
+            }
+        }
+
+
+        public static List<rates> ConvertJsonDataToRatesList(JsonModel jsondata)
+        {
+            Type jsonratesdata = jsondata.rates.GetType();
+            PropertyInfo[] ratesproperties = jsonratesdata.GetProperties();
+
+            List<rates> Rates = new List<rates>();
+
+            foreach (PropertyInfo property in ratesproperties)
+            {
+                Rates.Add(new rates
+                {
+                    Date = DateTime.Parse(jsondata.date),
+                    Quote = jsondata.@base,
+                    Base = property.Name,
+                    Rate = Double.Parse(property.GetValue(jsondata.rates, null).ToString())
+                });
+
+            }
+
+            return Rates;
+        }
+    }
+}
---
+++ Projects/WeSourceExam/WeSourceExam/HTMLView.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+
+namespace WeSourceExam
+{
+    class HTMLView
+    {
+        public static void CreateHTML(string viewbody)
+        {
+            
+            string html = WeSourceExam.Properties.Resources.Main.Replace("{BODY}", viewbody);
+            File.WriteAllText(@"Webview.html",html);            
+            
+        }   
+        
+        public static string CreateMainRows(int ID, string COUNTRYGROUP,double AMOUNT)
+        {
+            string MainRows;
+
+            MainRows = WeSourceExam.Properties.Resources.Row.Replace("{ROWID}",ID.ToString());
+            MainRows = MainRows.Replace("{COUNTRYGROUP}", COUNTRYGROUP);
+            MainRows = MainRows.Replace("{AMOUNT}", AMOUNT.ToString("0.##"));
+
+            return MainRows;
+        }
+
+        public static string CreateHiddenRows(int ID, string COUNTRY, double AMOUNT)
+        {
+            string MainRows;
+
+            MainRows = WeSourceExam.Properties.Resources.Hidden.Replace("{ROWID}", ID.ToString());
+            MainRows = MainRows.Replace("{COUNTRY}", COUNTRY);
+            MainRows = MainRows.Replace("{AMOUNT}", AMOUNT.ToString("0.##"));
+
+            return MainRows;
+        }
+
+        public static void OpenHTMLInBrowser()
+        {
+            var runningProcess = System.Diagnostics.Process.GetProcessesByName("chrome");
+            if (runningProcess.Length != 0)
+            {
+                System.Diagnostics.Process.Start(@"C:\Program Files\Google\Chrome\Application\chrome.exe", "Webview.html");
+                return;
+            }
+            runningProcess = System.Diagnostics.Process.GetProcessesByName("firefox");
+            if (runningProcess.Length != 0)
+            {
+                System.Diagnostics.Process.Start(@"C:\Program Files\Mozilla Firefox\firefox.exe", "Webview.html");
+                return;
+            }
+            runningProcess = System.Diagnostics.Process.GetProcessesByName("msedge");
+            if (runningProcess.Length != 0)
+            {
+                System.Diagnostics.Process.Start(@"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe", "Webview.html");
+                return;
+            }
+
+            Console.WriteLine("No Open Browser Detected - You can open the file in " + Path.GetFullPath("Webview.html"));
+            Console.ReadLine();
+        }
+
+    }
+}
