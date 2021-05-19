8ad8a4e010b55473f4da3d1742627adb6855c1b7
strikeroverlord <84431400+strikeroverlord@users.noreply.github.com>
2021-05-19 12:42:41 +0800

    Add files via upload

---
+++ Projects/WeSourceExam/WeSourceExam/RatesModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeSourceExam
+{
+    public class rates
+    {
+        public DateTime Date { get; set; }
+        public string Quote { get; set; }
+        public string Base  { get; set; }
+        public double Rate { get; set; }
+
+    }
+
+}
