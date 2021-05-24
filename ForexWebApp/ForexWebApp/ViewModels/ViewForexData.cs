71fd6817884a5308e1e279e170d0483e7fc311b8
strikeroverlord <84431400+strikeroverlord@users.noreply.github.com>
2021-05-24 17:27:14 +0800

    Add files via upload

---
+++ ForexWebApp/ForexWebApp/ViewModels/ViewForexData.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+namespace ForexWebApp
+{
+
+    public class ViewForexData
+    {
+        public List<CountryGroupEUR> SortedCountryGroup {get; set; }
+        
+    }
+
+
+
+}
